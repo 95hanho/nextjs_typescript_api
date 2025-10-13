c567fdf72e9d048febf4813c9905d975c2a102ab
HanHo <64001275+95hanho@users.noreply.github.com>
2025-10-13 09:50:10 +0900

    Conflict test2 (#5)
    * main push test4
    
    * Revert "main push test4"
    
    This reverts commit 2704f2d9fd4ea366e3834429d002a4f4d6e58ef3.
    
    * 12321
    
    * Revert "12321"
    
    This reverts commit 18c93c20e0ec6e036e907a940bf8fb70e8559837.
    
    * 2222

--- src/main/java/me/_hanho/nextjs_shop/buy/BuyService.java
+++ src/main/java/me/_hanho/nextjs_shop/buy/BuyService.java
@@ -119,6 +119,8 @@ public class BuyService {
 		// nextjs_shop_product_detail(상품상세옵션) stock(재고수), sales_count(판매수) 변경
 		List<ProductWithCouponsDTO> productWithCouponList = payRequest.getItems();
 		
+		// 여기서 충돌이 날것이여~
+		
 		// nextjs_shop_usercoupon(유저쿠폰) used(사용여부) 변경
 		// nextjs_shop_coupon(쿠폰) amount(수량) 변경
 	}
