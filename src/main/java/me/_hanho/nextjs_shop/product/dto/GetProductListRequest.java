ea3a210805386cdd34f9f49658307ae57ac82aa2
95hanho <hosung6674@naver.com>
2026-04-22 22:51:27 +0900

    제품 리스트 조회 변경

---
+++ src/main/java/me/_hanho/nextjs_shop/product/dto/GetProductListRequest.java
@@ -0,0 +1,29 @@
+package me._hanho.nextjs_shop.product.dto;
+
+import java.sql.Timestamp;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetProductListRequest {
+
+    @NotBlank(message = "sort는 필수입니다.")
+    private String sort; // POPULAR | LATEST | PRICE_LOW | PRICE_HIGH
+
+    @NotBlank(message = "popularPeriod는 필수입니다.")
+    private String popularPeriod; // DAYS_7 | DAYS_30 | YEAR_1 | ALL
+
+    @NotNull(message = "menuSubId는 필수입니다.")
+    private Integer menuSubId;
+
+    private Timestamp lastCreatedAt;
+    private Integer lastProductId;
+    private Integer lastPopularity;
+    private Integer lastPrice;
+}
---
+++ src/main/java/me/_hanho/nextjs_shop/product/dto/GetProductListResponse.java
@@ -0,0 +1,17 @@
+
+package me._hanho.nextjs_shop.product.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetProductListResponse {
+    private List<ProductListResponse> productList;
+    private boolean hasNext;
+    private ProductListCursorResponse nextCursor;
+}
