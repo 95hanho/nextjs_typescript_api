5302bc8f19bfc8eed9f1b27b8be9200e61de499c
95hanho <hosung6674@naver.com>
2026-04-22 09:33:33 +0900

    DB fileName 파일확장자 제거

--- src/main/java/me/_hanho/nextjs_shop/file/FileService.java
+++ src/main/java/me/_hanho/nextjs_shop/file/FileService.java
@@ -37,6 +37,7 @@ public class FileService {
 		}
 		String storeFileName = createStoreFileName(fileName);
 		String fileExtension = extractExt(fileName).toLowerCase();
+		String fileNameWithoutExt = removeExt(originalFileName);
 		String filePath = uploadDir + "/" + storeFileName;
 
 		// 이미지 파일인지 검증(파일 확장자 검증)
@@ -57,7 +58,7 @@ public class FileService {
 		saveFile(file, filePath);
 		// DB 저장
 		FileUploadRequest request = FileUploadRequest.builder()
-				.fileName(fileName)
+				.fileName(fileNameWithoutExt)
 				.storeName(storeFileName)
 				.fileExtension(fileExtension)
 				.filePath(filePath)
